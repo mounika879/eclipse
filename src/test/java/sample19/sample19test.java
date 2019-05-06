227d28ad3ae82796f95a5a4e36bb01bc63395fac
mounika <kodatimounika@gmail.com>
2019-05-06 13:10:58 +0530

    this is sample19

---
+++ src/test/java/sample19/sample19test.java
@@ -0,0 +1,14 @@
+package sample19;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sample19test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
